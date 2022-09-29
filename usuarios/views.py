6fc96e99e7f581d450664ea294013a1364f796bc
Antony Botello <antonybotello@gmail.com>
2022-09-28 20:57:28 -0500

    view

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,11 +1,34 @@
-from django.shortcuts import render
+from multiprocessing import context
+from django.shortcuts import redirect, render
+from usuarios.forms import UsuarioForm
+
+from usuarios.models import Usuario
 
 # Create your views here.
 
 
 def usuarios(request):
 
-    context={
+    usuarios= Usuario.objects.all()
 
+    context={
+        "usuarios":usuarios
     }
     return render(request,'usuarios/usuarios.html',context)
+
+def usuarios_crear(request):
+
+    if request.method == 'POST':
+        form= UsuarioForm(request.POST)
+        if form.is_valid():
+            form.save()
+            print("El usuario se guardo correctamente")
+            return redirect('usuarios')
+        else:
+            print("El usuario no se guardo")
+    else:
+        form= UsuarioForm()
+    context={
+        "form":form
+    }
+    return render(request,'usuarios/usuarios-crear.html',context)
